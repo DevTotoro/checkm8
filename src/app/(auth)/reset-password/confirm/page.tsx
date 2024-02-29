6f9d8fb71b2c57fae078893c35839817a2a3d75e
DevTotoro <totoro.softwaredev@gmail.com>
2024-02-29 16:18:32 +0200

    Add reset-password-confirm page

--- src/app/(auth)/reset-password/confirm/page.tsx
+++ src/app/(auth)/reset-password/confirm/page.tsx
@@ -4,15 +4,15 @@ export const metadata: Metadata = {
   title: 'checkm8 • Reset password',
 };
 
-const ResetPasswordPage = () => {
+const ResetPasswordConfirmPage = () => {
   return (
     <div className='flex flex-1 flex-col items-center justify-center gap-3'>
       <h1 className='text-3xl font-semibold'>checkm8</h1>
       <p className='text-md font-extralight'>Ticking Off Success</p>
 
-      <h5 className='mt-5 text-lg font-medium'>Reset password</h5>
+      <h5 className='mt-5 text-lg font-medium'>Reset password confirm</h5>
     </div>
   );
 };
 
-export default ResetPasswordPage;
+export default ResetPasswordConfirmPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/reset-password'],
+  publicRoutes: ['/', '/reset-password', '/reset-password/confirm'],
 });
 
 export const config = {
